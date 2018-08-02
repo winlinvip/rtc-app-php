8663717712db79e25bdf237f96b00f1d230aa279
winlin <winlin@vip.126.com>
2018-08-02 17:21:11 +0800

    Support recover for some OpenAPI error.

--- app/v1/login.php
+++ app/v1/login.php
@@ -11,9 +11,87 @@ class ChannelAuth
 	public $nonce;
 	public $timestamp;
 	public $channel_key;
-	// Debug Request ID
 	public $request_id;
 	public $recovered;
+}
+
+function RecoverForError($ex, $app_id, $channel_id)
+{
+	$fatal = False;
+	$request_id = '';
+
+	if ($ex instanceof ServerException) {
+		$request_id = $ex->getRequestId();
+	}
+
+	if ($ex instanceof ClientException) {
+		$code = $ex->getErrorCode();
+		if ($code === 'IllegalOperationApp') {
+			$fatal = True;
+		} else if (strpos($code, 'InvalidAccessKeyId') === 0) {
+			$fatal = True;
+		} else if ($code === 'SignatureDoesNotMatch') {
+			$fatal = True;
+		}
+	}
+
+	if ($fatal) {
+		throw $ex;
+	}
+
+	$recovered = 'RCV-' . uniqid();
+	error_log('Recover from ex ' . $ex . ', recovered=' . $recovered);
+
+	$auth = new ChannelAuth();
+	$auth->app_id = $app_id;
+	$auth->channel_id = $channel_id;
+	$auth->channel_key = $recovered;
+	$auth->nonce = $recovered;
+	$auth->timestamp = $recovered;
+	$auth->request_id = $request_id;
+	$auth->recovered = True;
+	return $auth;
+}
+
+function CreateChannel($app_id, $channel_id,
+	$region_id, $endpoint, $access_key_id, $access_key_secret)
+{
+	try {
+		$iClientProfile = DefaultProfile::getProfile(
+			$region_id, $access_key_id, $access_key_secret);
+		$client = new DefaultAcsClient($iClientProfile);
+
+		$request = new RTC\CreateChannelRequest();
+		$request->setAppId($app_id);
+		$request->setChannelId($channel_id);
+
+		// Strongly recomment to set the RTC endpoint,
+		// because the exception is not the "right" one if not set.
+		// For example, if access-key-id is invalid:
+		//      1. if endpoint is set, exception is InvalidAccessKeyId.NotFound
+		//      2. if endpoint isn't set, exception is SDK.InvalidRegionId
+		// that's caused by query endpoint failed.
+		// @remark SDk will cache endpoints, however it will query endpoint for the first
+		//      time, so it's good for performance to set the endpoint.
+		DefaultProfile::addEndpoint($region_id, $region_id, $request->getProduct(), $endpoint);
+
+		// Use HTTP, x3 times faster than HTTPS.
+		$request->setProtocol('http');
+
+		$response = $client->getAcsResponse($request);
+
+		$auth = new ChannelAuth();
+		$auth->app_id = $app_id;
+		$auth->channel_id = $channel_id;
+		$auth->channel_key = $response->ChannelKey;
+		$auth->nonce = $response->Nonce;
+		$auth->timestamp = $response->Timestamp;
+		$auth->request_id = $response->RequestId;
+		$auth->recovered = False;
+		return $auth;
+	} catch (Exception $ex) {
+		return RecoverForError($ex, $app_id, $channel_id);
+	}
 }
 
 function BuildToken($channel_id, $channel_key,
@@ -23,43 +101,6 @@ function BuildToken($channel_id, $channel_key,
 		. $user_id . $session . $nonce . $timestamp;
 	$token = hash('sha256', $s);
 	return $token;
-}
-
-function CreateChannel($app_id, $channel_id,
-	$region_id, $endpoint, $access_key_id, $access_key_secret)
-{
-	$iClientProfile = DefaultProfile::getProfile(
-		$region_id, $access_key_id, $access_key_secret);
-	$client = new DefaultAcsClient($iClientProfile);
-
-	$request = new RTC\CreateChannelRequest();
-	$request->setAppId($app_id);
-	$request->setChannelId($channel_id);
-
-	// Strongly recomment to set the RTC endpoint,
-	// because the exception is not the "right" one if not set.
-	// For example, if access-key-id is invalid:
-	//      1. if endpoint is set, exception is InvalidAccessKeyId.NotFound
-	//      2. if endpoint isn't set, exception is SDK.InvalidRegionId
-	// that's caused by query endpoint failed.
-	// @remark SDk will cache endpoints, however it will query endpoint for the first
-	//      time, so it's good for performance to set the endpoint.
-	DefaultProfile::addEndpoint($region_id, $region_id, $request->getProduct(), $endpoint);
-
-	// Use HTTP, x3 times faster than HTTPS.
-	$request->setProtocol('http');
-
-	$response = $client->getAcsResponse($request);
-
-	$auth = new ChannelAuth();
-	$auth->app_id = $app_id;
-	$auth->channel_id = $channel_id;
-	$auth->channel_key = $response->ChannelKey;
-	$auth->nonce = $response->Nonce;
-	$auth->timestamp = $response->Timestamp;
-	$auth->request_id = $response->RequestId;
-	$auth->recovered = False;
-	return $auth;
 }
 
 // Allow Cross-Origin Resource Sharing (CORS).
@@ -120,8 +161,13 @@ $channels = ReadObjects();
 
 if (!isset($channels->{$channel_url})) {
 	$auth = CreateChannel($app_id, $channel_id, $region_id, $endpoint, $access_key_id, $access_key_secret);
-	$channels->{$channel_url} = $auth;
-	WriteObject($channels);
+
+	// If recovered from error, we should never cache it,
+	// and we should try to request again next time.
+	if (!$auth->recovered) {
+		$channels->{$channel_url} = $auth;
+		WriteObject($channels);
+	}
 
 	// By default, write log to /var/log/apache2/error_log
 	$duration = round((microtime(True) - $starttime) * 1000);
